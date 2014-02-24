e8a4a15c2f808b7f6dcf6303c9f198892ddfbdfa
Katia Oleinik <koleinik@bu.edu>
2014-02-24 13:10:50 -0500

    Wrote code to open bip header and separated do_headers() from segment.c

---
+++ segment/do_headers.c
@@ -0,0 +1,75 @@
+
+#include <memory.h>
+#include <math.h>
+#include <string.h>
+
+#include "segment.h"
+#include "gethdrs.h"
+
+void
+do_headers(Spr, ifd, mfd)
+Seg_proc        Spr;
+int             ifd;
+int             mfd;
+{
+    int             band;	/* band index */
+    BIH_T         **i_bihpp;	/* -> input BIH */
+    BIH_T         **m_bihpp;	/* -> mask BIH */
+
+ /*
+  * read input BIH
+  */
+    i_bihpp = bihread(ifd);
+    if (i_bihpp == NULL) {
+	error("can't read basic image header for input file");
+    }
+    Spr->i_bihpp = i_bihpp;
+
+    Spr->nlines = hnlines(ifd);
+    if (Spr->nlines > MAXSHORT)
+	error("Image has too many (%d) lines\n", Spr->nlines);
+    Spr->nsamps = hnsamps(ifd);
+    if (Spr->nsamps > MAXSHORT)
+	error("Image has too many (%d) samps\n", Spr->nsamps);
+    Spr->nbands = hnbands(ifd);
+    if (Spr->nbands > MAXSHORT)
+	error("Image has too many (%d) bands\n", Spr->nbands);
+
+ /*
+  * Check that all bands are 8 bit
+  */
+    for (band = 0; band < Spr->nbands; band++) {
+	if (bih_nbytes(i_bihpp[band]) != 1 ||
+	    bih_nbits(i_bihpp[band]) != 8)
+	    error("Band #%d is %d bits per pixel\n",
+		  band,
+		  bih_nbits(i_bihpp[band]));
+    }
+
+ /*
+  * Skip other input headers
+  */
+    skiphdrs(ifd);
+
+ /*
+  * If there is a mask image, process its headers and make sure it is
+  * compatible with the input image.
+  */
+    if (sf_get(Spr, SF_MASK)) {
+	m_bihpp = bihread(mfd);
+	if (m_bihpp == NULL) {
+	    error("can't read basic image header for mask file");
+	}
+	if (hnbands(mfd) != 1)
+	    error("Mask image has %d bands", hnbands(mfd));
+	if (Spr->nlines != hnlines(mfd))
+	    error("Input and mask images have different number of lines");
+	if (Spr->nsamps != hnsamps(mfd))
+	    error("Input and mask images have different number of samples");
+	if (bih_nbytes(m_bihpp[0]) != 1)
+	    error("The mask image is not 1 byte per pixel");
+
+	skiphdrs(mfd);
+    }
+}
+
---
+++ segment/gdal_test/envi_read.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <gdal.h>
+#include "cpl_conv.h"
+
+int main(int argc, char ** argv){
+
+  GDALDatasetH hDataset=NULL;
+  char cmd[256];
+
+  char **papszMetadata = NULL;
+  int i;
+
+  // initializae GDAL library
+  GDALAllRegister();
+  
+  // make sure we have input and output file
+  if(argc <3) {
+    printf("usage: envi_read <inputfile> <outputfile>\n");
+    //return(0);
+    printf("Using default file names!\n");
+    sprintf(cmd,"gdal_translate -of ENVI -ot Byte -scale -co INTERLEAVE=BIP -co SUFFIX=ADD testsmall.bsq temp_byte_bip" );
+  }else{
+
+  // prepare a string for the system command
+    sprintf(cmd,"gdal_translate -of ENVI -ot Byte -scale -co INTERLEAVE=BIP -co SUFFIX=ADD %s %s", argv[1], argv[2] );
+  }
+
+  // convert input file
+  if (! system(cmd) )printf("Conversion of bsq to bip is unseccessful...\n");
+
+  // open bip file
+  hDataset = GDALOpen( "temp_byte_bip", GA_ReadOnly );
+  if (hDataset == NULL){
+    printf("Alas...\n");
+  } else{
+    printf("Success!\n");
+  }
+
+  // read metadata from bip file
+  papszMetadata = GDALGetMetadata( hDataset, "ENVI" );
+  if (papszMetadata == NULL){
+    printf("no metadata...\n");
+  } else{
+    printf("Read metadata!\n");
+
+    // print the matadata to the screen
+    for (i=0; i< 13; i++){
+      printf("%s\n",papszMetadata[i]);
+    }
+
+  }
+  
+  // close bip file 
+  GDALClose(hDataset);
+
+  return;
+
+}
